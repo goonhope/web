546c7b6265a228b792453348ec84166925ff0648
goonhope <50105128+goonhope@users.noreply.github.com>
2020-05-06 17:34:53 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+'''
+@FileName	:   __init__.py
+@Created     :   2020/05/06
+@Updated    :   2020/05/06
+@Author		:   goonhope@gmail.com
+@Function	:   
+'''
---
+++ urll.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+@FileName	 :   url.py 
+@Created     :   2020/05/04
+@Updated    :   2020/05/05
+@Author		  :   goonhope@gmail.com
+@Function	  :   临时中转
+'''
+
+from flask import Flask,render_template as tmp
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainpage():
+    return tmp("page.html",name=None)
+
+
+def made01(du):
+    name, url = du
+    @app.route(f'/{name}')
+    def page01():
+        return tmp("page.html",name=name,url=url)
+
+
+def made02(du):
+    name, url = du
+    @app.route(f'/{name}')
+    def page02():
+        return tmp("page.html",name=name,url=url)
+
+
+if __name__ == "__main__":
+    google = ("google", "http://www.google.com.hk/")
+    book = ("book","http://www.key007.com/e/action/ListInfo/?classid=61")
+    made01(google)
+    made02(book)
+    app.run(host='0.0.0.0', port=8080,debug=True)
